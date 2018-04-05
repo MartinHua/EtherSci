504dea0d7e414add46bee41bfb14d0dd40eed2b5
cchsu <cchsu@utexas.edu>
2018-04-05 16:00:24 -0500

    add segtree

---
+++ SegTree.py
@@ -0,0 +1,16 @@
+class Node:
+    def __init__(self,tx):
+        self.fee = tx["XX"]
+        self.blockNum
+class SegTree:
+    def __init__(self, n):
+        self.num = n
+        self.tree = [None]* self.num
+    def addNode(self, node):
+        return 0
+    def findMin(self, start, end):
+        return 0
+    def findMax(self, start, end):
+        return 0
+
+
